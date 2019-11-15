ae79ecea494aa349d88649805abe1702bbc3edc0
Don Newman <dnewman@talentpath.com>
2019-11-15 14:01:39 -0600

    couple of tests - ish

---
+++ server.test.js
@@ -0,0 +1,21 @@
+import { pipelineBareFunction } from "@babel/types";
+
+describe("Web Calc Functionality", async () => {
+const response = await post('2+2')    () => {
+      expect(new Calculator(2).add(3)).toBe(5);
+    };
+});
+
+describe("POST /'2+2", function() {
+    it('returns 4', function(done) {
+      request(app)  //but this uses express ;-;
+        .post("/'2+2'")
+        .set('Accept', 'application/json')
+        .expect('Content-Type', text/plain)
+        .expect(200)
+        .end(function(err, res) {
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
